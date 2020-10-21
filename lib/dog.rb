cb3e8253975c4e39d3a9964b527e7483a2905072
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 18:39:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,4 @@
+# defines Dog class
+# defines #initialize that accepts dog's name and optional breed argument default == "Mutt"
+# stores dog's name in @name variable
+# stores dog's breen in @breed variable
