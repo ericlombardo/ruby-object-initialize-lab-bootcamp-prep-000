3a88665ec7eb8e198406e6ae42837e03a6ce5d41
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 18:45:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 # defines Dog class
-# defines #initialize that accepts dog's name and optional breed argument default == "Mutt"
-# stores dog's name in @name variable
-# stores dog's breen in @breed variable
-
 class Dog
+  defines #initialize that accepts dog's name and optional breed argument default == "Mutt"
   def initialize(name, breed = "Mutt")
+    # stores dog's name in @name variable
     @name = name
+    # stores dog's breen in @breed variable
     @breed = breed
   end
 end
