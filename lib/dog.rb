ad2970471e367e764644c0410f8eb261e23e18dd
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 18:44:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,7 @@
 
 class Dog
   def initialize(name, breed = "Mutt")
-    def name=(name)
-      @name = name
-    end
-    def breed=(breed)
-      breed = @breed
-    end
+    @name = name
+    @breed = breed
   end
 end
